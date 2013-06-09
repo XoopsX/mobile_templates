b025fc8463dfabf5ff46c7749397db5507fb2a7b
nao-pon <hypweb@gmail.com>
2013-06-09 15:59:09 +0900

    change module directory to "mobile_templates"

--- html/modules/mobile_templates/xoops_version.php
+++ html/modules/mobile_templates/xoops_version.php
@@ -1,12 +1,12 @@
 <?php
 
-$modversion['name'] = 'ktai_default_templates';
+$modversion['name'] = 'Mobile(ktai_default) templates';
 $modversion['version'] = 1.01;
 $modversion['description'] = 'Templates set under the ktai_default theme';
 $modversion['author'] = 'XCL XOOPS X (ten) DISTRIBUTION TEAM';
 $modversion['credits'] = 'https://github.com/XoopsX';
 $modversion['license'] = 'GPL';
-$modversion['dirname'] = 'ktai_default_templates';
+$modversion['dirname'] = 'mobile_templates';
 $modversion['image'] = 'module_icon.png';
 $modversion['hasAdmin'] = 0;
 $modversion['hasMain'] = 0;
