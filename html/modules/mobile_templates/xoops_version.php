32115f043ac216076238872a09d767223971ace3
naao <naao@mbg.nifty.com>
2014-03-09 01:42:27 +0900

    ver.1.02 : modified for d3diary over 0.41.0

--- html/modules/mobile_templates/xoops_version.php
+++ html/modules/mobile_templates/xoops_version.php
@@ -1,7 +1,7 @@
 <?php
 
 $modversion['name'] = 'Mobile(ktai_default) templates';
-$modversion['version'] = 1.01;
+$modversion['version'] = 1.02;
 $modversion['description'] = 'Templates set under the ktai_default theme';
 $modversion['author'] = 'XCL XOOPS X (ten) DISTRIBUTION TEAM';
 $modversion['credits'] = 'https://github.com/XoopsX';
